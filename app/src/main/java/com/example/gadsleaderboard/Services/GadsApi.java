6cbea47cd8498d4d65c428fdede452362a4aa123
yolita <gyolandem@gmail.com>
2020-09-10 01:47:24 +0300

    I edited the post form url from test to gadsproject form

--- app/src/main/java/com/example/gadsleaderboard/Services/GadsApi.java
+++ app/src/main/java/com/example/gadsleaderboard/Services/GadsApi.java
@@ -19,7 +19,7 @@ public interface GadsApi {
     Call<List<LearnerModel>> getHrs();
 
     @FormUrlEncoded
-    @POST("https://docs.google.com/forms/d/e/1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/viewform")
+    @POST("https://docs.google.com/forms/d/e/1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse")
     Call<Void> save(
             @Field("entry.1877115667") String firstname,
             @Field("entry.2006916086")  String lastname,
